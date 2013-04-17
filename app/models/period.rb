bb85a23d3ea7d96431667b32d902e0d4e1350c33
Oleg Haidul <oleghaidul@gmail.com>
2013-04-17 22:14:54 +0300

    n+1 littlefix

--- app/models/period.rb
+++ app/models/period.rb
@@ -54,7 +54,7 @@ class Period < ActiveRecord::Base
     end
 
     def self.send_notification
-      @periods_end = Period.where{end_date > Date.today}.where{ end_date <= Date.today+1}
+      @periods_end = Period.where{end_date > Date.today}.where{ end_date <= Date.today+1}.includes(:user_calendar)
       @periods_end.each do |period|
         if period.user_calendar.user.email == "xx999xx@mail.ru" #test
           Notifier.send_notification( period.user_calendar.user.email,
@@ -63,7 +63,7 @@ class Period < ActiveRecord::Base
                                       false).deliver
         end
       end
-      @periods_start = Period.where{start_date > Date.today }.where{ start_date <= Date.today+1}
+      @periods_start = Period.where{start_date > Date.today }.where{ start_date <= Date.today+1}.includes(:user_calendar)
       @periods_start.each do |period_start|
         if period_start.user_calendar.user.email == "xx999xx@mail.ru" #test
           Notifier.send_notification( period_start.user_calendar.user.email,
